6cdbe20ee0f27e8599123441f8e64d9431bf2b9e
Ameya Phansalkar <aphansal123@gmail.com>
2019-06-16 20:00:23 -0700

    worked on Talker

---
+++ Assets/scripts/Talker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Talker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// temporary debugging code
+		Vector3 position = transform.position;
+		position.x = 1;
+		
+		// say something
+		print("I am the best teddy bear ever!");
+	}
+	
+}
